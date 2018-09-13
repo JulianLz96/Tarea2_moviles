112298adcbb36637e7a3151a0c07179433c33dd9
Julián López <jj_lz96@hotmail.com>
2018-09-13 16:16:49 -0500

    Buttons square

--- app/src/main/java/com/iteso/tarea02/Activity_main.java
+++ app/src/main/java/com/iteso/tarea02/Activity_main.java
@@ -1,6 +1,7 @@
 package com.iteso.tarea02;
 
 import android.content.res.Configuration;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -14,6 +15,10 @@ public class Activity_main extends AppCompatActivity {
 
     ImageButton like;
     RadioGroup colors;
+    Button save;
+    Button sm;
+    private static final String KEY_STATE_BUTTON = "state_button";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +26,13 @@ public class Activity_main extends AppCompatActivity {
 
         like = findViewById(R.id.like_button);
         colors = findViewById(R.id.radio_group);
+        save = findViewById(R.id.button_save);
+        sm = findViewById(R.id.radio_button_m);
+
+        if(savedInstanceState != null){
+            CharSequence textSaved = savedInstanceState.getCharSequence(KEY_STATE_BUTTON);
+            save.setText(textSaved);
+        }
 
         like.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,5 +45,31 @@ public class Activity_main extends AppCompatActivity {
                 plusone.show();
             }
         });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(save.getText().toString() == getResources().getString(R.string.add_to_cart)) {
+                    Snackbar.make(view, getResources().getString(R.string.added_to_cart), Snackbar.LENGTH_LONG)
+                            .setAction("Undo", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    save.setText(R.string.add_to_cart);
+                                }
+                            })
+                            .show();
+
+                    save.setText(R.string.added_to_cart);
+                } else {
+                    Snackbar.make(view, R.string.already_in_cart, Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putCharSequence(KEY_STATE_BUTTON, save.getText());
     }
 }
